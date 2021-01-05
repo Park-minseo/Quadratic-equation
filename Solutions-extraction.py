6129fca34ca238f355b70c1809d81072a44f0067
Park-minseo <jangmur41@gmail.com>
2021-01-05 20:25:45 +0900

    판별식이 0 미만일때 오류발생 수정, x²과 x의 계수가 0일때 표시 생략하도록 수정

--- Solutions-extraction.py
+++ Solutions-extraction.py
@@ -1,32 +1,44 @@
-from math import *
+import math
 
-a = 1
-b = 10
-c = 21
-근1= (b*-1 + sqrt(b ** 2-4 * a * c)) / 2*a
-근2= (b*-1 - sqrt(b ** 2-4 * a * c)) / 2*a
+a=float(input("x²의 계수를 입력하세요: "))
+b = float(input("x의 계수를 입력해주세요: "))
+c = float(input("상수항의 계수를 입력해주세요: "))
+근1 = 근2 = 0
+판별식 = b ** 2 - 4 * a * c
 
-if b ** 2-4 * a * c == 0:
+def 근의공식1(a, b, c):
+    근1= (b*-1) + math.sqrt(판별식) / 2*a
+    return 근1
+def 근의공식2(a, b, c):
+    근2= (b*-1) - math.sqrt(판별식) / 2*a
+    return 근2
+
+if 판별식 >= 0:
+    근1=근의공식1(a,b,c)
+    근2=근의공식2(a,b,c)
     if a==1:
         if b==1:
-            print("x^2+x+" +str(c) +"=0의 근은 "+ str(근1))
+            if 판별식 == 0:
+                print("x²+x{0:+}=0의 근은 {1}".format(c, 근1))
+            else:
+                print("x²+x{0:+}=0의 근은 {1}, {2}".format(c, 근1,근2))
         else:
-            print("x^2+" + str(b) + "x+" +str(c) +"=0의 근은 "+ str(근1))
+            if 판별식 == 0:
+                print("x²{0:+}x{1:+}=0의 근은 {2}".format(b,c, 근1))
+            else:
+                print("x²{0:+}x{1:+}=0의 근은 {2}, {3}".format(b,c, 근1,근2))
     else:
         if b==1:
-            print(str(a) + "x^2+x+" +str(c) +"=0의 근은 "+ str(근1))
+            if 판별식 == 0:
+                print("{0}x²+x{1:+}=0의 근은 {2}".format(a,c,근1))
+            else:
+                print("{0}x²+x{1:+}=0의 근은 {2}, {3}".format(a,c,근1,근2))
         else:
-            print(str(a) + "x^2" + str(b) +"x+" +str(c) +"=0의 근은 "+ str(근1))
-else:
-    if a==1:
-        if b==1:
-            print("x^2+x+" +str(c) +"=0의 근은 "+ str(근1) +"," +str(근2))
-        else:
-            print("x^2+" + str(b) + "x+" +str(c) +"=0의 근은 "+ str(근1) +"," +str(근2))
-    else:
-        if b==1:
-            print(str(a) + "x^2+x+" +str(c) +"=0의 근은 "+ str(근1) +"," +str(근2))
-        else:
-            print(str(a) + "x^2" + str(b) +"x+" +str(c) +"=0의 근은 "+ str(근1) +"," +str(근2))
+            if 판별식 == 0:
+                print("{0}x²{1:+}x{2:+}=0의 근은 {3}".format(a,b,c, 근1))
+            else:
+                print("{0}x²{1:+}x{2:+}=0의 근은 {3}. {4}".format(a,b,c, 근1,근2))
+elif b ** 2 -4 * a* c < 0:
+    print("허근은 해당 프로그램에서 지원되지 않습니다.")
 
 
